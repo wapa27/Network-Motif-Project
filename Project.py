e866e1977ec5d8394fe9ac81946c099befbe3d98
wapa27 <wapa1274@colorado.edu>
2019-04-14 17:44:26 -0600

    Not too pretty but works

--- Project.py
+++ Project.py
@@ -13,52 +13,61 @@ class node:
 
 
 def main():
-    print(sys.argv[1])
-    # inputfile = ''
     graph = []
-    with open(sys.argv[1]) as f:
-        blank_list = []
-        for i in range(3):
+    with open(sys.argv[1]) as f:  # open argument 2 (parent graph)
+        blank_list = []  # if making new node, we need blank adj list
+        for i in range(3):  # first two lines we don't want
             line = f.readline()
-            if(i == 2):
+            if(i == 2):             # building our first two nodes / first connection
                 root, adj = line.split()
                 root = node(root, blank_list)
                 adj = node(adj, blank_list)
                 graph.append(root)
+                graph.append(adj)
+                adj.adj_list.append(graph[0])
                 graph[0].adj_list.append(adj)
         for line in f:
             blank_list = []
             root_status = False         # root node NOT in graph
             adj_status = False          # adj node NOT in graph
             root, adj = line.split()
-            for j in range(len(graph)-1):
+            for j in range(len(graph)):
                 if root == graph[j].id:  # root already a node in graph
                     root_status = True
-                    for k in range(len(graph)-1):  # check if adjacent node in graph too
+                    for k in range(len(graph)):  # check if adjacent node in graph too
                         if adj == graph[k].id:  # if adj node in graph already
                             adj_status = True
                             # append adj node to root node list
+                            graph[k].adj_list.append(graph[j])
                             graph[j].adj_list.append(graph[k])
                             break
                     if adj_status == False:  # if adjacent node not already in graph
                         adj = node(adj, blank_list)
                         graph[j].adj_list.append(adj)
+                        adj.adj_list.append(graph[j])
                         graph.append(adj)
             if root_status == False:  # root not in graph yet
                 root = node(root, blank_list)
                 graph.append(root)
-                for k in range(len(graph)-1):  # check if adjacent node is in graph too
+                for k in range(len(graph)):  # check if adjacent node is in graph too
                     if adj == graph[k].id:  # if adj node in graph already
                         adj_status = True
                         # append adj node to root node list
                         graph[j].adj_list.append(graph[k])
+                        graph[k].adj_list.append(graph[j])
 
                 if adj_status == False:  # if adjacent node not already in graph
                     adj = node(adj, blank_list)
                     root.adj_list.append(adj)
+                    adj.adj_list.append(root)
                     graph.append(adj)
-
-    print(graph[0].adj_list[0].id)  # should index 0 of first node's adj list
+    count = 0
+    tmp = []
+    # quick test (printing count of edges --> given at top of .net file)
+    for x in range(0, len(graph)):
+        if graph[x].id not in tmp:
+            tmp.append(graph[x])
+    print(len(graph))
 
 
 main()
