1b7fb7ffe7782f0de3ac2075e1a48664e5bc55c0
elpa8934 <42749435+elpa8934@users.noreply.github.com>
2019-05-01 14:01:39 -0600

    Add files via upload

---
+++ GrandChallenge.py
@@ -0,0 +1,96 @@
+from adjListGraph import *
+
+
+def compatible(G, H, toTry, possMatch):
+	hPrime = Graph([])
+	newMatch = [[],[]]
+	#make a copy of possMatch using a for loop initialization
+	for i in range(len(possMatch[0])):
+		newMatch[0].append(possMatch[0][i])
+		newMatch[1].append(possMatch[1][i])
+	newMatch[0].append(toTry)
+	newMatch[1].append(H.nodeList[len(newMatch[0])-1].id) #len(possMatch)-1 is index of h
+	print(newMatch[1])
+	print(newMatch[0])
+	#make a copy of H using a for loop initialization
+	for i in range(len(newMatch[1])): #index of node IDs in G
+		hPrime.nodeList.append(H.nodeList[i])
+	#replace nodes in hPrime with mapping from newMatch[0]
+	for n in range(len(hPrime.nodeList)): #loop thru hPrime's nodeList
+		for m in range(len(newMatch[1])): #loop the newMatch[1] list
+			check = hPrime.nodeList[n]
+			if check.id == newMatch[1][m]: #if node in hPrime has same ID as node in newMatch[1]
+				check.id = newMatch[0][m] #change node id to mapped value from newMatch[0]
+				for a in range(len(check.adj_list)): #loop thru adj list to change values
+					for b in range(len(newMatch[1])):
+						adjCheck = check.adj_list[a]
+						if adjCheck == newMatch[1][b]:
+							check.adj_list[a] = newMatch[0][b]
+			#hPrime.nodeList[n] = check
+
+			#hPrime.nodeList[i] = newMatch[0][i]
+	print("G: ")
+	G.PrintGraph()
+	print("HPrime: ")
+	hPrime.PrintGraph()
+
+	
+
+
+	#return newMatch
+
+def recursiveSearch(G,H,g,possMatch,IsomorphList):
+	print("g.id = " + g.id)
+	for n in g.adj_list:
+		print("node = " + n)
+		possAdd = compatible(G, H, n, possMatch)
+		if (possAdd != None):
+			if len(possAdd[1]) == len(H.nodeList):
+				IsomorphList.append(possAdd) #?
+				return 1 #?
+			else:
+				recursiveSearch(G,H, n, possAdd, IsomorphList)
+	return 0
+
+def IsomorphicExtentions(G,H,g):
+	IsomorphList = [] # number of found subgraphs from node g
+		#print(current.id)
+		#G:graph, H:motif, current: node object of int n, g,h[0]: suppose g and h[0] are compatible, IsomorphList: record completed maps
+	toopy = [[g.id],[H.nodeList[0].id]]
+	hold = recursiveSearch(G,H,g,toopy,IsomorphList)
+	return IsomorphList
+
+'''
+def IsomorphicExtentions(G,H,g,possMatch):
+	IsomorphList = [] # number of found subgraphs from node g
+	for n in g.adj_list:
+		current = G.FindNode(n)
+		#print(current.id)
+		#G:graph, H:motif, current: node object of int n, g,h[0]: suppose g and h[0] are compatible, IsomorphList: record completed maps
+		toopy = ([g],[H.nodeList[0]])
+		hold = recursiveSearch(G,H,current,toopy,IsomorphList)
+	return IsomorphList
+'''
+
+def FindSubgraphInstances(G,H):
+	Instances = 0 # Final number of subgraphs
+	for g in G.nodeList:
+		possMatch = [] ### do we need this?
+		Instances += len(IsomorphicExtentions(G,H,g))
+		#check for repetitions ?
+	return Instances 
+
+
+if __name__ == "__main__":
+	graph = LoadGraph("eznetwork.txt")
+	# print(len(graph.nodeList))
+	subgraph = LoadGraph("ezmotif.txt")
+	G = sortByDegree(graph)
+	H = sortByDegree(subgraph)
+	
+	G.PrintGraph()
+	H.PrintGraph()
+	print("--------")
+	FindSubgraphInstances(G,H)
+
+
