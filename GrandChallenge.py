d1edbcb7421bc4516d7a442b4af77e391cb2c1cf
Willie Chew <37847148+williewlchew@users.noreply.github.com>
2019-05-02 00:22:11 -0600

    I Lied. Part 2

--- GrandChallenge.py
+++ GrandChallenge.py
@@ -29,21 +29,35 @@ def compatible(G, H, toTry, possMatch):
 
 	#replace nodes in hPrime with mapping from newMatch[0]
 	for n in range(len(hPrime.nodeList)): #loop thru hPrime's nodeList
+		
 		check = hPrime.nodeList[n]
 		for m in range(len(newMatch[1])): #loop the newMatch[1] list
 			if check.id == newMatch[1][m]: #if node in hPrime has same ID as node in newMatch[1]
 				check.id = newMatch[0][m] #change node id to mapped value from newMatch[0]
+		'''
 		for a in range(len(check.adj_list)): #loop thru adj list to change values
 			for b in range(len(newMatch[1])):
 				adjCheck = check.adj_list[a]
 				if adjCheck == newMatch[1][b]:
 					check.adj_list[a] = newMatch[0][b]
+		'''
+		for a in range(len(check.adj_list)): 
+			mapped = False
+			for b in range(len(newMatch[1])):
+				adjCheck = check.adj_list[a]
+				if adjCheck == newMatch[1][b]:
+					check.adj_list[a] = newMatch[0][b]
+					mapped = True
+			if mapped == False:
+				check.adj_list.pop(a)
+
 	if (gCompat(G, hPrime)):
 		return newMatch
 	else:
 		return None
 
 def recursiveSearch(G,H,g,possMatch,IsomorphList):
+	print("g.id = " + g.id)
 	for n in g.adj_list:
 		possAdd = compatible(G, H, n, possMatch)
 		if (possAdd != None):
@@ -51,14 +65,14 @@ def recursiveSearch(G,H,g,possMatch,IsomorphList):
 				IsomorphList.append(possAdd)
 				print(possAdd)
 			else:
-				recursiveSearch(G,H, n, possAdd, IsomorphList)
+				recursiveSearch(G,H, G.FindNode(n), possAdd, IsomorphList)
 
 def IsomorphicExtentions(G,H,g):
 	IsomorphList = [] # number of found subgraphs from node g
 		#print(current.id)
 		#G:graph, H:motif, current: node object of int n, g,h[0]: suppose g and h[0] are compatible, IsomorphList: record completed maps
 	toopy = [[g.id],[H.nodeList[0].id]]
-	hold = recursiveSearch(G,H,g,toopy,IsomorphList)
+	recursiveSearch(G,H,g,toopy,IsomorphList)
 	return IsomorphList
 
 def FindSubgraphInstances(G,H):
@@ -71,9 +85,9 @@ def FindSubgraphInstances(G,H):
 
 
 if __name__ == "__main__":
-	graph = LoadGraph("eznetwork.txt")
+	graph = LoadGraph("trinetwork.txt")
 	# print(len(graph.nodeList))
-	subgraph = LoadGraph("ezmotif.txt")
+	subgraph = LoadGraph("trimotif.txt")
 	G = sortByDegree(graph)
 	H = sortByDegree(subgraph)
 	
